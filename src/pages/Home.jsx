1fc774e823549617b6019e514523b16b28a6bd18
Habsa Abdirizack <habsa.abdirizack@student.moringaschool.com>
2024-11-15 00:07:33 +0300

    assignment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,36 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import MovieCard from "../components/MovieCard"; /
 
 function Home() {
+  const [movies, setMovies] = useState([]);
+
+ 
+  useEffect(() => {
+    fetch("http://localhost:4000/movies")
+      .then((response) => response.json())
+      .then((data) => setMovies(data))
+      .catch((error) => console.error("Error fetching movies:", error));
+  }, []);
+
+ 
+  if (movies.length === 0) {
+    return <h1>Loading movies...</h1>;
+  }
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Info goes here! */}
+        <h1>Home Page</h1>
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
       </main>
     </>
   );
-};
+}
 
 export default Home;
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,16 +1,38 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 function Movie() {
+  const [movie, setMovie] = useState({});
+  const { id } = useParams(); 
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${id}`)
+      .then((response) => response.json())
+      .then((data) => setMovie(data))
+      .catch((error) => console.error("Error fetching movie:", error));
+  }, [id]);
+
+  if (!movie.title) {
+    return <h1>Loading...</h1>; 
+  }
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movie.title}</h1>
+        <p>{movie.time}</p>
+        <div>
+          {movie.genres.map((genre, index) => (
+              <span key={index}>{genre} </span>)
+              )}
+        </div>
       </main>
     </>
   );
-};
+}
 
 export default Movie;
